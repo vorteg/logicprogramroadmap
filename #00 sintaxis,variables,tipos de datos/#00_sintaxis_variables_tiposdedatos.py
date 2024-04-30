9d0f40b7c96fca0d6314034a146b240b2ebe61b7
Victor Ortega <51211089+vorteg@users.noreply.github.com>
2024-04-30 20:52:47 +0000

    00 Ejercicio Completo

---
+++ #00 sintaxis,variables,tipos de datos/#00_sintaxis_variables_tiposdedatos.py
@@ -0,0 +1,24 @@
+# https://python.org/
+ 
+# Comentario en una linea
+
+"""
+    Comentario en varias lineas
+"""
+
+'''
+ Comentario en varias lineas
+
+'''
+
+
+my_variable = "Mi variable"
+MY_CONSTANT = ("Mi constante")   
+
+my_int = 1
+my_float = 1.2
+my_str = 'texto'
+my_bool = True
+
+
+print("Hola, Python!")
